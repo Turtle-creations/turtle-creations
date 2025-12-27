ee457b1d4831a14821c2bfb002b177d3b99fcd20
Turtle-creations <kr.amitsoren@gmail.com>
2025-12-28 00:39:04 +0530

    Update script.js
    New dinamic change

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Turtle Creations website loaded successfully!");
+const comics = ["images/comic1.jpg","images/comic2.jpg"];
+let index = 0;
+const slide = document.getElementById("comicSlide");
+
+function nextSlide(){
+index=(index+1)%comics.length;
+slide.src=comics[index];
+}
+
+function prevSlide(){
+index=(index-1+comics.length)%comics.length;
+slide.src=comics[index];
+}
+
+// Mobile swipe
+let startX=0;
+slide.addEventListener("touchstart",e=>startX=e.touches[0].clientX);
+slide.addEventListener("touchend",e=>{
+let endX=e.changedTouches[0].clientX;
+if(startX-endX>50) nextSlide();
+if(endX-startX>50) prevSlide();
+});
